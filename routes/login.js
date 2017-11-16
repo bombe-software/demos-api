5e852bb82f3f93b20f36fe9e1f3933488fa32dc4
Vicroni <drasa_tec@hotmail.com>
2017-11-15 23:02:47 -0600

    Kerberos en login completo

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 //Configurar de la base de datos
 var mysql = require('mysql');
+var CryptoJS = require("crypto-js");
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -18,16 +19,31 @@ con.connect(function (err) {
 /*
  * POST /login
  */
+
 exports.post = function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' });
-  console.log(request.body);
-  con.query('select * from Usuario where email = ? && contrasena = ?',
-    [
-      request.body.CorreoElectronico,
-      request.body.Contrasena
-    ],
-    function Query(error, rows) {
-      response.end(JSON.stringify(rows));
-    }
-  );
+  if(request.body.correo_electronico !="404"){
+    let ip = request.headers['cf-connecting-ip'] || request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    con.query('select  * from Usuario  where email = ?',
+        [
+          request.body.correo_electronico
+        ],
+        function Query(error, rows) {
+          if(JSON.stringify(rows) != '[]'){
+            let usuario = JSON.parse(JSON.stringify(rows[0]));
+            let bytes  = CryptoJS.AES.decrypt(request.body.ticket, 'jaiba');
+            let ticket = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+            if(ticket.direccion_cliente == ip && ticket.route == request.originalUrl){
+              console.log(usuario);
+              response.end(JSON.stringify(usuario));
+            }else{
+              response.end("404");
+            }
+          }	
+        }
+    );
+  }else{
+    response.end("404");
+  }
+	
 };
